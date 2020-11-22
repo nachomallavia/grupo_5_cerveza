f83785bda38045e170e82dd8676bf7b11a3d0da4
Shan-Mamotiuk <ianmamotiuk2001@gmail.com>
2020-11-21 23:28:39 -0300

    form del productCreate

--- src/app.js
+++ src/app.js
@@ -5,10 +5,11 @@ const path = require("path");
 const cartRouter = require ('./routes/cartRouter');
 const productsRouter = require ('./routes/productRouter');
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '../public')));
-
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function(req,res){
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,3 +1,18 @@
+const multer = require('multer');
+const path = require('path');
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, '../../public/uploads'))
+    },
+    filename: function(req, file, cb){
+        cb(null, req.body.pname + "_picture"+ path.extname(file.originalname))
+    }
+})
+
+var upload = multer({storage: storage})
+
+
 const express = require('express');
 const router = express.Router();
 const productController = require ('../controllers/productController');
@@ -7,8 +22,9 @@ router.get('/', productController.all);
 router.get('/detail', productController.detail);
 
 router.get('/create', productController.create);
+router.post('/create', upload.any(), productController.createForm)
 
 router.get('/edit', productController.edit);
-
+router.post('/edit', upload.any(), productController.editForm)
 
 module.exports = router;
