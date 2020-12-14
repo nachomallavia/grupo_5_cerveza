e9d4f6c4f8ed46e39f4205b06066deb320bf70e3
joseia <anadonjose@gmail.com>
2020-12-14 20:13:47 -0300

    agregué creacion de users y eliminé node modules innecesario en src

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,17 @@
+const fs = require('fs');
 const path = require('path');
+const bcryptjs = require('bcryptjs');
+
+
+let usuarios = fs.readFileSync(path.join(__dirname, '../database/users.json'), 'utf8');
+usuarios = JSON.parse(usuarios);
+
+let ultimoIdUser = 0;
+for (let i = 0; i < usuarios.length; i++){
+    if (ultimoIdUser < usuarios[i].id){
+        ultimoIdUser = usuarios[i].id;
+    }
+}
 
 const controller = {
     register: function(req, res) {
@@ -6,6 +19,19 @@ const controller = {
     },
     login: function(req, res) {
         res.render('users/login.ejs')
+    },
+    save: function(req, res) {
+        
+        let nuevoUsuario = {
+            id: ultimoIdUser + 1,
+            email: req.body.email,
+            avatar: req.file.filename,
+            birthdate: req.body.fecha,
+            password: bcryptjs.hashSync(req.body.password, 12)
+        };
+        usuarios.push(nuevoUsuario)
+            fs.writeFileSync(path.join(__dirname, '../database/users.json'), JSON.stringify(usuarios, null, 4));
+            res.redirect('/')
     }
 };
     module.exports = controller;
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,8 +1,24 @@
 const express = require ('express');
 const router = express.Router();
 const userController = require ('../controllers/userController');
+const multer = require('multer');
+const path = require('path');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../../public/uploads/avatars'));
+    },
+    filename: function (req, file, cb) {
+        console.log()
+        cb(null, req.body.email + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+   
+var upload = multer({ storage: storage })
 
 router.get('/register', userController.register );
+router.post('/register',upload.single('avatar'), userController.save );
+
 router.get('/login', userController.login );
 
 module.exports = router;
