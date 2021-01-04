b6492a46155a3dd74c85fe03dc0253d66edc9778
Shan-Mamotiuk <ianmamotiuk2001@gmail.com>
2021-01-04 19:43:42 -0300

    Formulario de creación de combos (incompleto)

--- src/controllers/comboController.js
+++ src/controllers/comboController.js
@@ -9,6 +9,8 @@ let combos = JSON.parse(fs.readFileSync(path.join(__dirname,'../database/combos.
 let categorias = fs.readFileSync(path.join(__dirname,'../database/categories.json'),'utf-8');
     categorias = JSON.parse(categorias);
 
+let items = []; 
+
 let ultimoIdCombo = 0;
 for (let i = 0; i < combos.length; i++){
     if (ultimoIdCombo < combos[i].id){
@@ -24,10 +26,24 @@ const controller = {
         res.render('products/comboAdminList');
     },
     Create: function(req, res){
-        res.render('products/comboCreate');
+        res.render('products/comboCreate',{'productos': productos});
     },
     CreateForm: function(req, res){
-        
+        for(let i = 1; i <= productos.length; i++){
+            var item = `req.body.pid${i}`;
+            if(item > 0){
+                items.push({nombreProducto: item});
+            };
+        };
+        res.send(item);
+        // combos.push({
+        //     id: ultimoIdCombo + 1,
+        //     name: req.body.pname,
+        //     desc: req.body.pdesc,
+        //     price: req.body.pprice,
+        //     items:,
+        //     image: req.files[0].filename
+        // })
     },
     edit: function(req, res){
         res.render('products/comboEdit');
