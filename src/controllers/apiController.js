75edbe7f22d34de0b023a0db19d6cd8cdc2b343b
Shan-Mamotiuk <ianmamotiuk2001@gmail.com>
2021-03-09 19:47:21 -0300

    Api completa

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,47 +1,48 @@
-const path = require ('path');
 const db = require ('../database/models/index.js');
-let users = ''
+
 const controller = {
     
     usersList:function(req,res){
-        db.Users.findAll({attributes: ['email','id','name','avatar']})
-        .then(function(listado){
-            users = listado;
-            users.map(function(user){
-                user.avatar = "/api/users/"+user.id;
-            })
-            console.log(users);
+        db.Users.findAll({attributes: ['email','id','name']})
+        .then(function(users){
+            for(let i = 0; i < users.length; i++){
+                users[i].dataValues["detail"] = `/api/users/${users[i].id}` 
+            }
             return res.status(200).json({
                 cantidad: users.length,
                 users
             })  
         })
-        // users.forEach(function (user) {
-        //     user.avatar= "/api/users/"+user.id;
-        // });
     },
+
     usersDetail:function(req,res){
         db.Users.findByPk(req.params.id,{attributes: ['email','id','name','avatar']})
         .then(function(user){
-            user.avatar = "/uploads/avatars/"+user.avatar; 
+            user.avatar = `/uploads/avatars/${user.avatar}`; 
             return res.status(200).json({
                 user
             })
         })
     },
+
     productsList: function(req,res){
         db.Products.findAll({include:[{association:"category"}],attributes: ['id','name','description','image']})
-        .then(function(dBproductos){
-            productos = dBproductos;
+        .then(function(productos){
+            for(let i = 0; i < productos.length; i++){
+                productos[i].dataValues["detail"] = `/api/products/${productos[i].id}`
+
+            }
             db.sequelize.query("SELECT t2.name, count(t1.id) as cant FROM products t1, categories t2 where t1.id_category=t2.id group by id_category")
             .then(function(categorias) {
-                productos.map(function(producto){
-                    producto.image = "/api/products/"+producto.id;
-                })
+                let categories = {}
+                for(let i = 0; i < categorias[0].length; i++){
+                    categories[categorias[0][i].name] = categorias[0][i].cant;
+                }
                 return res.status(200).json({
                     cantidad: productos.length,
-                    cantidadPorCategoría: categorias[0],
+                    cantidadPorCategoría: categories,
                     productos,
+
                 })
             })
         })
@@ -49,12 +50,17 @@ const controller = {
     productsDetail: function(req,res){
         db.Products.findByPk(req.params.id,{include:[{association:"category"},{association:"srm_index"},{association:"maker"},{association:"format"}],attributes: ['id','name','description','image','abv','ibu','price']})
         .then(function(product){
-            let relaciones = [];
-            relaciones.push(product.category)
-            relaciones.push(product.format)
-            relaciones.push(product.maker)
-            relaciones.push(product.srm_index)
-            product.image = "/uploads/"+product.image; 
+            let relations = [];
+            product.category.dataValues["relationName"] = "Category"
+            product.srm_index.dataValues["relationName"] = "Srm_index"
+            product.maker.dataValues["relationName"] = "Maker"
+            product.format.dataValues["relationName"] = "Format"
+            relations.push(product.category)
+            relations.push(product.format)
+            relations.push(product.maker)
+            relations.push(product.srm_index)
+            
+            product.image = `/uploads/${product.image}`; 
             return res.status(200).json({
                 id: product.id,
                 name: product.name,
@@ -63,7 +69,7 @@ const controller = {
                 image: product.image,
                 abv: product.abv,
                 ibu: product.ibu,
-                relaciones
+                relations
             })
         })
     }
